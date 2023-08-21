73403a33d1005b79dd4cda6f78f74d6cbe8fa32f
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-21 13:14:52 -0700

    main header file

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_char(char c);
+int print_string(char *c);
+#endif
