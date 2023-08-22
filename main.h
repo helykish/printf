7634f1b5506bab7bcc93c691e58a8f6689c48b54
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-22 13:22:19 -0700

    main header file

--- main.h
+++ main.h
@@ -8,6 +8,8 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int print_char(char c);
+int printf_string(va_list val);
 int print_string(va_list args);
+int printf_unsigned(va_list args);
 void print_int(int num);
 #endif
