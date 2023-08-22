3981454a53291143d3c7c0fee382694237375018
AhmedTahaD <ahmedtaha2893@gmail.com>
2023-08-22 14:23:53 -0700

    Printf function

--- _printf.c
+++ _printf.c
@@ -9,6 +9,10 @@ int _printf(const char *format, ...)
 	int i, length = 0, str_len;
 	va_list args;
 
+	if (format == NULL)
+	{
+		return (-1);
+	}
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
 	{
