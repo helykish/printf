02890afd16a42e9e55d7131c422e52e962abd127
helykish <111018650+helykish@users.noreply.github.com>
2023-08-22 21:39:37 +0300

    Update printf_string.c

--- printf_string.c
+++ printf_string.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * printf_string - print a string.
  * @val: argumen t.
- * Return: the length of the string.
+ * Return: length of the string.
  */
 
 int printf_string(va_list val)
